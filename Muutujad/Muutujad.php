aaa8b60559871e33b08837963fd00ca4a68f6413
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2019-09-30 21:34:46 +0300

    Lahendasin kõik alaülesanded.

--- Muutujad/Muutujad.php
+++ Muutujad/Muutujad.php
@@ -1,16 +1,35 @@
 <?php
+
 /*  Ülesanne 1
     Hanna-Liisa Vilms
     30.09.19
 */
 
 $nimi="Hanna-Liisa Vilms";
-$vanus="19";
+$vanus=19;
 $sugu="naine";
 
 echo 'Tere '.$nimi.'!<br>';
 echo 'Sinu vanus: '.$vanus.'<br>';
 echo 'Sinu sugu: '.$sugu.'<br>';
-echo .$nimi.' on '.$vanus.' aastane '.$sugu.'<br>';
+echo $nimi.' on '.$vanus.' aastane '.$sugu.'<br>';
+echo '&nbsp;<br>';
+
+echo "\"It's Alright, Ma (I'm Only Bleeding)\" - Bob Dylan <br>";
+echo '&nbsp;<br>';
+
+echo '&nbsp;\ | /<br>';
+echo '@@ <br>';
+echo '&nbsp;&nbsp;* <br>';
+echo '\\"""/<br><br>';
+echo '&nbsp;<br>';
+
+echo '&nbsp;&nbsp;&nbsp;\___/<br>';
+echo '&nbsp;&nbsp;(@,@)<br>';
+echo '&nbsp;(&nbsp;(&nbsp;|&nbsp;|&nbsp;)&nbsp;)<br>';
+echo '__,,_,,__<br>';
+
+
+
 
 ?>
