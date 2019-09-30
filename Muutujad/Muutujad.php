9f88c5bada26dadb50e926b8e02930fd47e30d39
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2019-09-30 18:14:58 +0300

    Tegin 1.ülesandest 1.taski ära.

---
+++ Muutujad/Muutujad.php
@@ -0,0 +1,16 @@
+<?php
+/*  Ülesanne 1
+    Hanna-Liisa Vilms
+    30.09.19
+*/
+
+$nimi="Hanna-Liisa Vilms";
+$vanus="19";
+$sugu="naine";
+
+echo 'Tere '.$nimi.'!<br>';
+echo 'Sinu vanus: '.$vanus.'<br>';
+echo 'Sinu sugu: '.$sugu.'<br>';
+echo .$nimi.' on '.$vanus.' aastane '.$sugu.'<br>';
+
+?>
