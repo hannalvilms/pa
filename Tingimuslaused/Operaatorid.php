ba7a2efce8ee22e77d699df0a27306b3aa09b049
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2019-10-11 08:54:04 +0300

    operaatiorid

---
+++ Tingimuslaused/Operaatorid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * file name: operaatorid.php;
+ * autor: Hanna-Liisa Vilms;
+ * date: 10.10.2019;
+ */
+$x = 3;
+$y = 2;
+// loogilised operaatorid
+var_dump($x == $y);
+echo '<hr>';
+var_dump($x != $y);
+echo '<hr>';
+var_dump($x < $y);
+echo '<hr>';
+var_dump($x > $y);
+echo '<hr>';
+var_dump($x <= $y);
+echo '<hr>';
+var_dump($x >= $y);
+echo '<hr>';
