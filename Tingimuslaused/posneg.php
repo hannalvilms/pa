8e1526beab0672fb3e45861f3e4e5a0518e163a2
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2019-10-11 08:55:48 +0300

    Positiivne-negatiivne

---
+++ Tingimuslaused/posneg.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * file name: posneg.php;
+ * autor: Hanna-Liisa Vilms;
+ * date: 10.10.2019;
+ */
+
+$arv = 0;
+if ($arv > 0) {
+    echo $arv . ' on positiivne<br>';
+} else if ($arv < 0) {
+    echo $arv . ' on negatiivne<br>';
+} else {
+    echo $arv . ' on null<br>';
+}
+echo 'see on programmi lõpp';
