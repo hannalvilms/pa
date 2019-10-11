4364c65adb430dc997bec25aee0232a9aab90989
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2019-10-11 09:47:49 +0300

    arvamismäng

---
+++ Tingimuslaused/switch.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * file name: switch.php;
+ * autor: Hanna-Liisa Vilms;
+ * date: 11.10.2019;
+ */
+
+$etteAntudArv = 20;
+//lubame kasutajal arvata
+$kasutajaArv = 15;
+
+
+//võrdlus
+if($etteAntudArv == $kasutajaArv) {
+    echo 'Tubli! Arvasid ära<br>';
+}
+ else {
+     //arvud ei ole võrdsed|kas vahemik arvude vahel väiksem v võrdne 5-ga|| kui arvud negatiivsed, teisendame postiivseks.
+     $vahemik = abs($kasutajaArv) - $etteAntudArv;
+     if ($vahemik <= 5) {
+         echo 'Veel ei arvanud ära, kuid oled lähedal!<br>';
+     }
+     if ($kasutajaArv > $etteAntudArv) {
+         echo $kasutajaArv . ' on suurem, kui ette antud arv. <br>';
+     } else {
+         echo $kasutajaArv . ' on väiksem, kui ette antud arv. <br>';
+     }
+
+ }
